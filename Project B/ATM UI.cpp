6b91d95eecc82a1e15f22526c583f753a1fc7aa3
SOAChishti <soachishti@outlook.com>
2015-04-07 20:14:39 +0500

    Initial Commit

---
+++ Project B/ATM UI.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+int main () {
+    cout << endl << endl << endl;
+    cout << setw(55) << " _________________________ " << endl;
+    cout << setw(55) << "/                         \\" << endl;
+    cout << setw(55) << "|  PRO BANK               |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "|  1234 5672 1892 1728    |" << endl;
+    cout << setw(55) << "|  NAME:                  |" << endl;
+    cout << setw(55) << "|                    ---- |" << endl;
+    cout << setw(55) << "|  Valid: 1/15       VISA |" << endl;
+    cout << setw(55) << "|                    ---- |" << endl;
+    cout << setw(55) << "\\_________________________/" << endl;
+    
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    system("pause");
+    return 0;
+}
---
+++ Project B/Login UI.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+bool logged = false;
+
+void cls() {
+	system("cls");
+}
+
+void goToXY( int x, int y )
+{
+	COORD coord = {x,y};
+	SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ),coord);
+}
+
+string userInput (int type) {
+	char ch;
+	string input;
+	ch = _getch();
+	while (ch != 13) {
+		if (type == 1 && ch >= 'a' && ch <= 'z') { //username
+			cout << ch;
+			input += ch;
+		}
+		else if (type == 2)  { //password
+			cout << "*";
+			input += ch;
+		}
+		else if (type == 3 && ch >= '0' && ch <= '9') { //Role
+			if (ch == '1' || ch == '2' || ch == '3') {
+				cout << ch;
+				input += ch;
+				break;
+			}
+			
+		}
+		ch = _getch();
+	}
+	return input;
+}
+
+void loginUI () {
+	string username, password;
+	int role;
+
+	goToXY(20,5);
+	cout << "------------------------------------";
+	goToXY(20,7);
+	cout << "          *** LOGIN ***             ";
+
+
+	goToXY(25,10);
+	cout << "USERNAME: ";
+	goToXY(25,11);
+	cout << "PASSWORD: ";
+	goToXY(25,12);
+	cout << "ROLE:";
+
+	goToXY(20,16);
+	cout << "------------------------------------";
+
+	goToXY (35,10);
+	username = userInput(1);
+	goToXY (35,11);
+	password = userInput(2);
+	goToXY (31,12);
+	role = atoi(userInput(3).c_str());
+	goToXY (35,15);
+	
+	//cout << username << " " << password << " " << role;
+	logged = true;
+
+}
+
+void menu () {
+	cout << "-------------------" << "\n\n" 
+		 << " Hello, " << "Owais     |" << "\n" 
+		 << "-------------------" << "\n\n" 
+		 << " Information      -" << "\n" 
+		 << "-------------------" << "\n\n" 
+		 << " Transfer Money   |" <<  "\n"
+		 << "-------------------" << "\n\n" 
+		 << " Change Password  |" << "\n" 
+		 << "-------------------" << "\n\n" 
+		 << " Change PIN       |" << "\n" 
+		 << "-------------------" << "\n\n" 
+		 << " Logout           |" << "\n" 
+		 << "-------------------";
+
+
+}
+
+int main() {
+	loginUI ();
+	if (logged == true) {
+		cls();
+		menu();
+	}
+	return 0;
+}
