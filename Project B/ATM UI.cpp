2f4d2b2f0dd118aebd86d87583c558d841d4420b
Syed Chishti <soac@outlook.com>
2015-04-08 06:54:04 +0000

    More functonality

--- Project B/ATM UI.cpp
+++ Project B/ATM UI.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
+#include <string>
+#include <windows.h>
 using namespace std;
 
-int main1 () {
-    cout << endl << endl << endl;
+HANDLE hCon;
+	enum Color { DARKBLUE = 1, DARKGREEN, DARKTEAL, DARKRED, DARKPINK, DARKYELLOW, GRAY, DARKGRAY, BLUE, GREEN, TEAL, RED, PINK, YELLOW, WHITE };
+
+	void SetColor(Color c) {
+		if (hCon == NULL) {
+			hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+		}
+		SetConsoleTextAttribute(hCon, c);
+
+		//SetColor(YELLOW);
+		//	cout << "X\b";
+	}
+
+
+void goToXY(short int x, short int y) {
+		COORD coord = { x, y };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+string getUsername() {
+	string username;
+	
+	cout << endl << endl << endl;
     cout << setw(55) << " _________________________ " << endl;
     cout << setw(55) << "/                         \\" << endl;
-    cout << setw(55) << "|  PRO BANK               |" << endl;
+    cout << setw(55) << "|  PROJECT B              |" << endl;
     cout << setw(55) << "|                         |" << endl;
     cout << setw(55) << "|  1234 5672 1892 1728    |" << endl;
     cout << setw(55) << "|  NAME:                  |" << endl;
@@ -15,20 +38,57 @@ int main1 () {
     cout << setw(55) << "|  Valid: 1/15       VISA |" << endl;
     cout << setw(55) << "|                    ---- |" << endl;
     cout << setw(55) << "\\_________________________/" << endl;
-    
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
+
+	goToXY(37,8);
+	cin >> username;
+	return username;
+} 
+
+string getPIN() {
+	string PIN;
+	cout << endl << endl << endl;
+    cout << setw(55) << " _________________________ " << endl;
+    cout << setw(55) << "/                         \\" << endl;
+    cout << setw(55) << "|  PROJECT B              |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "|      PIN:               |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "\\_________________________/" << endl;
+
+	goToXY(40,8);
+	cin >> PIN;
+	return PIN;	
+}
+
+
+void accountBlocked() {
+	cout << endl << endl << endl;
+    cout << setw(55) << " _________________________ " << endl;
+    cout << setw(55) << "/                         \\" << endl;
+    cout << setw(55) << "|  PROJECT B              |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+	SetColor(RED);
+	cout  << setw(55) << "| ~~~ ACCOUNT BLOCKED ~~~ |" << endl;
+    SetColor(WHITE);
+	cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "|                         |" << endl;
+    cout << setw(55) << "\\_________________________/" << endl;
+}
+
+
+
+int main1 () {
+	string username = getUsername();
+	system("cls");
+	string PIN = getPIN();
+	system("cls");
+	accountBlocked();
+	//cout << username;
     system("pause");
     return 0;
 }
