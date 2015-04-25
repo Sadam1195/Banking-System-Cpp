5dcbdd74a3a8f0e56cd9a6887a9eacf1e58e1f84
Syed Chishti <soac@outlook.com>
2015-04-25 05:44:45 +0000

    Login UI.cpp edited online with Bitbucket

--- Project B/Login UI.cpp
+++ Project B/Login UI.cpp
@@ -1,95 +1,100 @@
+#ifndef GRAPHICS_CPP
+#define GRAPHICS_CPP
+#include "Graphics.cpp"
+#endif
+
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
 #include <string>
-#include "Graphics.cpp"
 
 using namespace std;
 
-bool logged = false;
-
-string userInput (int type) {
-	char ch;
-	string input;
-	ch = _getch();
-	while (ch != 13) {
-		if (type == 1 && ch >= 'a' && ch <= 'z') { //username
-			cout << ch;
-			input += ch;
-		}
-		else if (type == 2)  { //password
-			cout << "*";
-			input += ch;
-		}
-		else if (type == 3 && ch >= '0' && ch <= '9') { //Role
-			if (ch == '1' || ch == '2' || ch == '3') {
+class loginUI
+{
+private:
+	bool logged = false;
+	string userInput(int type) {
+		char ch;
+		string input;
+		ch = _getch();
+		while (ch != 13) {
+			if (type == 1 && ch >= 'a' && ch <= 'z') { //username
 				cout << ch;
 				input += ch;
-				break;
 			}
-			
+			else if (type == 2)  { //password
+				cout << "*";
+				input += ch;
+			}
+			else if (type == 3 && ch >= '0' && ch <= '9') { //Role
+				if (ch == '1' || ch == '2' || ch == '3') {
+					cout << ch;
+					input += ch;
+					break;
+				}
+
+			}
+			ch = _getch();
 		}
-		ch = _getch();
+		return input;
 	}
-	return input;
-}
 
-void loginUI () {
-	string username, password;
-	int role;
+	void login() {
+		string username, password;
+		int role;
 
-	Graphics::goToXY(20, 5);
-	cout << "------------------------------------";
-	Graphics::goToXY(20, 7);
-	cout << "          *** LOGIN ***             ";
+		Graphics::goToXY(20, 5);
+		cout << "------------------------------------";
+		Graphics::goToXY(20, 7);
+		cout << "          *** LOGIN ***             ";
 
 
-	Graphics::goToXY(25, 10);
-	cout << "USERNAME: ";
-	Graphics::goToXY(25, 11);
-	cout << "PASSWORD: ";
-	Graphics::goToXY(25, 12);
-	cout << "ROLE:";
+		Graphics::goToXY(25, 10);
+		cout << "USERNAME: ";
+		Graphics::goToXY(25, 11);
+		cout << "PASSWORD: ";
+		Graphics::goToXY(25, 12);
+		cout << "ROLE:";
 
-	Graphics::goToXY(20, 16);
-	cout << "------------------------------------";
+		Graphics::goToXY(20, 16);
+		cout << "------------------------------------";
 
-	Graphics::goToXY(35, 10);
-	username = userInput(1);
-	Graphics::goToXY(35, 11);
-	password = userInput(2);
-	Graphics::goToXY(31, 12);
-	role = atoi(userInput(3).c_str());
-	Graphics::goToXY(35, 15);
-	
-	//cout << username << " " << password << " " << role;
-	logged = true;
+		Graphics::goToXY(35, 10);
+		username = userInput(1);
+		Graphics::goToXY(35, 11);
+		password = userInput(2);
+		Graphics::goToXY(31, 12);
+		role = atoi(userInput(3).c_str());
+		Graphics::goToXY(35, 15);
 
-}
-
-void menu () {
-	cout << "-------------------" << "\n\n" 
-		 << " Hello, " << "Owais     |" << "\n" 
-		 << "-------------------" << "\n\n" 
-		 << " Information      -" << "\n" 
-		 << "-------------------" << "\n\n" 
-		 << " Transfer Money   |" <<  "\n"
-		 << "-------------------" << "\n\n" 
-		 << " Change Password  |" << "\n" 
-		 << "-------------------" << "\n\n" 
-		 << " Change PIN       |" << "\n" 
-		 << "-------------------" << "\n\n" 
-		 << " Logout           |" << "\n" 
-		 << "-------------------";
-
-
-}
-
-int main3() {
-	loginUI ();
-	if (true) {
-		Graphics::cls();
-		menu();
+		//cout << username << " " << password << " " << role;
+		logged = true;
 	}
-	return 0;
-}
+
+	void menu() {
+		cout << "-------------------" << "\n\n"
+			<< " Hello, " << "Owais     |" << "\n"
+			<< "-------------------" << "\n\n"
+			<< " Information      -" << "\n"
+			<< "-------------------" << "\n\n"
+			<< " Transfer Money   |" << "\n"
+			<< "-------------------" << "\n\n"
+			<< " Change Password  |" << "\n"
+			<< "-------------------" << "\n\n"
+			<< " Change PIN       |" << "\n"
+			<< "-------------------" << "\n\n"
+			<< " Logout           |" << "\n"
+			<< "-------------------";
+
+
+	}
+public:
+	void run() {
+		login();
+		if (true) {
+			Graphics::cls();
+			menu();
+		}
+	}
+};
