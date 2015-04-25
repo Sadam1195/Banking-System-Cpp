e10608d852846afd5e5fb1cba41f816c450ed4d0
zack123 <zerkshaban@live.com>
2015-04-25 10:53:21 +0500

    New

--- Project B/Source.cpp
+++ Project B/Source.cpp
@@ -3,42 +3,48 @@
 #include <stdio.h>    
 #include <string>     // to_string()
 
-#include "Graphics.cpp"
-#include "Bank.cpp"
+#include "Login_UI.cpp"
+#include "ATM_UI.cpp"
 
 using namespace std;
 
-int main2()
+int main()
 {
+	//atmUI atm;
+	//atm.run();
+
+	loginUI online;
+	online.run();
+
 
 	// Registration
-	Bank branch;
-	userDetail user;
+	//Bank branch;
+	//userDetail user;
 
-	user.name = "Syed Owais Ali Chishti";
-	user.username = "owais";
-	user.password = "pass";
-	user.pin = Bank::randomPIN();
-	user.role = 1;
-	user.atmBlocked = 0;
-	
+	//user.name = "Syed Owais Ali Chishti";
+	//user.username = "owais";
+	//user.password = "pass";
+	//user.pin = Bank::randomPIN();
+	//user.role = 1;
+	//user.atmBlocked = 0;
+
 	/*
 	if(branch.addUser(user) == true)// if return false mean user already exist with that username
 	{
-		cout << "User Added";
+	cout << "User Added";
 	}
 	else {
-		cout << "Unable to add user.";
+	cout << "Unable to add user.";
 	}
 	*/
 
 	/*
-	// Login    
+	// Login
 	if (branch.login("owais", "pass", 1)) {
-		cout << "Logged in";
+	cout << "Logged in";
 	}
 	else {
-		cout << "Sorry!!! unable to log in.";
+	cout << "Sorry!!! unable to log in.";
 	}
 	*/
 
@@ -53,6 +59,7 @@ int main2()
 	//branch.addBalance(username, -500);
 	//cout << branch.getBalance(username);
 	//branch.deleteUser(username);
+
 
 	system("pause");
 	return 0;
