9ee06696809e80467727d3d93d1c8bebd1a35187
SOAChishti <soachishti@outlook.com>
2015-04-26 22:21:15 +0500

    Front Page Added

--- Project B/Source.cpp
+++ Project B/Source.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>    
 #include <string>     // to_string()
 
+
 #include "Login_UI.cpp"
 #include "ATM_UI.cpp"
 
@@ -10,12 +11,68 @@ using namespace std;
 
 int main()
 {
+	cout
+		<< endl << endl
+		<< "\t  ____                     _                 _             ____  " << endl
+		<< "\t |  _ \\   _ __    ___     (_)   ___    ___  | |_          | __ ) " << endl
+		<< "\t | |_) | | '__|  / _ \\    | |  / _ \\  / __| | __|         |  _ \\ " << endl
+		<< "\t |  __/  | |    | (_) |   | | |  __/ | (__  | |_          | |_) |" << endl
+		<< "\t |_|     |_|     \\___/   _/ |  \\___|  \\___|  \\__|         |____/ " << endl
+		<< "\t                        |__/                                     " << endl;
 
-	//atmUI atm;
-	//atm.run();
+	cout << "\t\t\t\t\t\t    By Owais (P14-6011) " << endl
+		 << "\t\t\t\t\t\t       Zerk (P14-6012)" << endl;
 
-	loginUI online;
-	online.run();
+
+
+
+
+
+
+
+
+
+	Graphics::goToXY(30,15);
+	cout << "ATM";
+	Graphics::goToXY(30, 16);
+	cout << "e-Banking";
+
+
+	int x = 45,
+		y = 15;
+	Graphics::goToXY(x, y);
+	cout << "X\b";
+
+	char ch;
+	int count = 0;
+
+	while (ch = _getch()) {
+		if (ch == 80 && count < 1) { // DOWN
+			cout << " \b";
+			Graphics::goToXY(x, y += 1);
+			cout << "X\b";
+			count++;
+		}
+		if (ch == 72 && count != 0) { // UP
+			cout << " \b";
+			Graphics::goToXY(x, y -= 1);
+			cout << "X\b";
+			count--;
+		}
+		if (ch == 13) {
+			switch (count) {
+			case 0:
+				atmUI atm;
+				atm.run();
+				break;
+			case 1:
+				loginUI online;
+				online.run();
+				break;
+			}
+			break;
+		}
+	}
 
 	/*
 	Bank branch;
